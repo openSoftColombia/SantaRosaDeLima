7f21798f59d602852e9e36946f987069cde3a70d
Michael Rodriguez <73129506+maicolrg11@users.noreply.github.com>
2021-03-27 21:34:00 -0500

    FIX: new

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 //component
 import HeaderLayout from './components/layout/header/HeaderLayout';
-import MainContent from './components/layout/mainContent/MainContent';
 import FooterLayout from './components/layout/footer/FooterLayout';
 import PublicRoutes from './router/Public.Routes';
 import SocialMedia from './components/layout/socialMedia/SocialMedia';
@@ -13,10 +12,8 @@ function App() {
     <React.Fragment>
       <HeaderLayout />
       <SocialMedia />
-      <MainContent>
-        <PublicRoutes />
-        {/* <PrivateRoutes propa={props} /> */}
-      </MainContent>
+      <PublicRoutes />
+      {/* <PrivateRoutes propa={props} /> */}
       <FooterLayout />
     </React.Fragment>
   );
--- src/components/layout/mainContent/MainContent.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const MainContent = ({ children }) => {
-  return (<main className="main"> {children} </main>)
-}
-
-export default MainContent;
